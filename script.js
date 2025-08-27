d9056a1d86db2220684e5a3359920c1e1bd21925
KimTofuu <152471516+KimTofuu@users.noreply.github.com>
2025-08-28 00:05:34 +0800

    initial push

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('profileBtn').onclick = function() {
+    var profile = document.getElementById('profile');
+    profile.style.display = 'block';
+    setTimeout(function() {
+        profile.classList.add('show');
+    }, 10);
+    document.getElementById('profileBtn').style.display = 'none';
+};
+
+document.getElementById('viewMore').onclick = function() {
+    var moreDeets = document.getElementById('moreDeets');
+    moreDeets.style.display = 'block';
+    setTimeout(function() {
+        moreDeets.classList.add('show');
+    }, 10);
+    document.getElementById('viewMore').style.display = 'none';
+};
